b2ddf66579ffc7d7211214b12b18d46723ab5ed1
Rafael <rafaelbarros.df@gmail.com>
2021-12-29 13:07:03 -0300

    :construction: Global Exception Handler

--- src/main/java/academy/devdojo/webflux/SpringWebfluxEssentialsApplication.java
+++ src/main/java/academy/devdojo/webflux/SpringWebfluxEssentialsApplication.java
@@ -2,6 +2,8 @@ package academy.devdojo.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
 import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
@@ -14,6 +16,11 @@ public class SpringWebfluxEssentialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxEssentialsApplication.class, args);
+	}
+
+	@Bean
+	public WebProperties.Resources getResoucers() {
+		return new WebProperties.Resources();
 	}
 
 }
