3c01e2c32c646b474a2dfdff3edc9ffa2ed83e34
Rafael <rafaelbarros.df@gmail.com>
2021-12-29 13:14:35 -0300

    :construction: Retrieving trace param from the URI

--- src/main/java/academy/devdojo/webflux/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/academy/devdojo/webflux/exception/handler/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -19,6 +20,9 @@ import reactor.core.publisher.Mono;
 
 import java.util.Map;
 import java.util.Optional;
+
+import static java.util.Optional.of;
+import static org.springframework.boot.web.error.ErrorAttributeOptions.defaults;
 
 /**
  * created by:
@@ -43,13 +47,20 @@ public class GlobalExceptionHandler extends AbstractErrorWebExceptionHandler {
     }
 
     private Mono<ServerResponse> formatErrorResponse(ServerRequest request) {
-        Map<String, Object> errorAttributesMap = getErrorAttributes(request, ErrorAttributeOptions.
-                of(ErrorAttributeOptions.Include.STACK_TRACE));
+        String query = request.uri().getQuery();
+        ErrorAttributeOptions errorAttributeOptions = isTraceEnabled(query) ?
+                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE) : defaults();
+
+        Map<String, Object> errorAttributesMap = getErrorAttributes(request, errorAttributeOptions);
         int status = (int) Optional.ofNullable(errorAttributesMap.get("status")).orElse(500);
         return ServerResponse
                 .status(status)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(errorAttributesMap));
+    }
+
+    private boolean isTraceEnabled(String query) {
+        return !StringUtils.isEmpty(query) && query.contains("trace=true");
     }
 }
 
