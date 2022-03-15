50ee2e7b8e256e98aabf8a5a14830bd05c2352fb
Rafael <rafaelbarros.df@gmail.com>
2022-03-15 12:41:55 -0300

    :construction: Reactive Security R2DBC Postgres pt 01

---
+++ src/main/java/academy/devdojo/webflux/domain/DevDojoUser.java
@@ -0,0 +1,64 @@
+package academy.devdojo.webflux.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * created by:
+ *
+ * @author rafael barros for DevDusCorre <rafaelbarros.softwareengineer@gmail.com> on 15/03/2022
+ */
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("tb_devdojo_user")
+public class DevDojoUser implements UserDetails {
+
+    @Id
+    private Integer id;
+    private String name;
+    private String username;
+    private String password;
+    private String authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(authorities.split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,19 @@ CREATE TABLE tb_anime
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+
+--------------------------------------------------------
+--  DDL for Table tb_devdojo_user
+--------------------------------------------------------
+
+DROP TABLE IF EXISTS tb_devdojo_user;
+CREATE TABLE tb_devdojo_user
+(
+    id serial NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    authorities VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id)
+);
